bea1786bceee5fca7f82bf8594ca016643d7afde
Cédric Laubacher <cedric@laubacher.io>
2020-12-23 21:07:41 +0100

    More comments

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -4,9 +4,11 @@ public class BubbleSort {
 
 	public static void sort(int[] arr) {
 		 
+		//loop through all pairs
 		 for (int i=0;i<arr.length;i++) {
+			 //ignore one more element at the end, as the biggest element has bubbled up
 			 for (int j=0;j<arr.length-i-1;j++) {
-				 System.out.print(j);
+				 //if element is bigger than its successor, swap them
 				 if (arr[j]>arr[j+1]) {
 					 ServiceClass.swap(arr, j, j+1);
 				 }
