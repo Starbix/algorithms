0ef31d6380233b6f7d445fd9771057c32f7a6ea2
Cédric Laubacher <cedric@laubacher.io>
2020-12-27 20:36:28 +0100

    Fix MergeSort and add comments

--- src/sorting/MergeSort.java
+++ src/sorting/MergeSort.java
@@ -9,8 +9,10 @@ public class MergeSort implements Sortable {
 	}
 	
 	static void mergeSort(int[] arr, int l, int r) {
+		// divide and conquer: we sort half of the given subarray until given a single element
 		if (l<r) {
 			int m = (l+r)/2;
+
 			mergeSort(arr, l, m);
 			mergeSort(arr, m+1, r);
 			merge(arr, l, r, m);
@@ -18,13 +20,18 @@ public class MergeSort implements Sortable {
 	}
 	
 	static void merge(int[] arr, int l, int r, int m) {
-		int[] b = new int[r-l];
+		// temporary sorted array
+		int[] b = new int[r-l+1];
 		
+		//indeces we iterate over
 		int i = l;
-		int j = m;
+		int j = m+1;
 		int k = 0;
 		
-		while (i<m&&j<r) {
+		// starting at the start of each subarray, we compare the elements
+		// and add the smaller one to b[]
+		// until all elements of one array have been copied to b[]
+		while (i<=m&&j<=r) {
 			if (arr[i]<=arr[j]) {
 				b[k] = arr[i++];
 			} else {
@@ -34,14 +41,18 @@ public class MergeSort implements Sortable {
 			k++;
 		}
 		
-		while (i<m) {
+		// copy the remaining elements
+		
+		while (i<=m) {
 			b[k++] = arr[i++];
 		}
-		while (j<r) {
+		while (j<=r) {
 			b[k++] = arr[j++];
 		}
 		
-		for (int g=l;g>=r;g--) {
+		
+		// update the original array
+		for (int g=l;g<=r;g++) {
 			arr[g] = b[g-l];
 		}
 		
