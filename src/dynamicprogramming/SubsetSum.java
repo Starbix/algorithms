9eefcf9e3896968efb6546131e8065c0687ed61b
Cédric Laubacher <cedric@laubacher.io>
2021-01-31 15:31:22 +0100

    Make 3-Partition more readable

--- src/dynamicprogramming/SubsetSum.java
+++ src/dynamicprogramming/SubsetSum.java
@@ -1,7 +1,5 @@
 package dynamicprogramming;
 
-import java.util.Arrays;
-
 public class SubsetSum {
 
 	static boolean DEBUG = false;
@@ -35,9 +33,13 @@ public class SubsetSum {
 
 		for (int k=1;k<=n;k++) {
 			for (int s=1;s<=z;s++) {
+				// k-th integer is bigger than s => can't be used yet
 				if (A[k-1]>s) {
 					dp[k][s] = dp[k-1][s];
 				} else {
+					// does partition of size s using integers 1..k exist?
+					// it exists if a partition of size s using fewer integers is possible
+					// or if a partition of size s-A[k-1] exists
 					dp[k][s] = dp[k-1][s]||dp[k-1][s-A[k-1]];
 				}
 			}
@@ -55,24 +57,22 @@ public class SubsetSum {
 		z /= 3;
 
 		int n = A.length;
+
+		//dp[k][s][t] <=> there exist two disjunct partitions of size s and t using the first k elements
 		boolean[][][] dp = new boolean[n+1][z+1][z+1];
 
+		// there always exists a partition of size 0
 		dp[0][0][0] = true;
 
+		// iterate through all elements
 		for (int k=1;k<=n;k++) {
 			int num = A[k-1];
 
 			for (int s=0;s<=z;s++) {
 				for (int t=0;t<=z;t++) {
-					if (num<=s&&num<=t) {
-						dp[k][s][t] = dp[k-1][s][t] || dp[k-1] [s] [t-num] || dp[k-1] [s-num] [t];
-					} else if (num<=s) {
-						dp[k][s][t] = dp[k-1][s][t] || dp[k-1] [s-num] [t];
-					} else if (num<=t) {
-						dp[k][s][t] = dp[k-1][s][t] || dp[k-1] [s] [t-num];
-					} else {
-						dp[k][s][t] = dp[k-1][s][t];
-					}
+					dp[k][s][t] = dp[k-1][s][t]
+							|| ((num<=s)? dp[k-1][s-num] [t]:false)
+							|| ((num<=t)? dp[k-1][s]     [t-num]:false);
 				}
 			}
 		}
